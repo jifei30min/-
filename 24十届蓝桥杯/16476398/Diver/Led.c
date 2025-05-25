45971a17048f47fe781bb3f94a112d84f66b257b
jifei <2750664768@qq.com>
2025-05-25 17:37:20 +0800

    Add files via upload

---
+++ 24十届蓝桥杯/16476398/Diver/Led.c
@@ -0,0 +1,19 @@
+#include <Led.H>
+
+//addr:是要亮哪个灯(0-7) enable:使能标志位,0不使能(熄灭) 1为使能(点亮)
+void Led_Disp(unsigned char addr, unsigned char enable)
+{ 
+	static unsigned char temp = 0x00;
+	static unsigned char temp_old = 0xff;//记录上一次的状态，用于优化IO操作
+	if(enable) 
+		temp |= 0x01 << addr;
+	else  
+		temp &= ~(0x01 << addr);
+	if(temp != temp_old)
+	{	
+		P0 = ~temp;
+		P2 = P2 & 0X1F | 0X80;
+		P2 &= 0X1F;
+		temp_old = temp;		
+	}
+}
---
+++ 24十届蓝桥杯/16476398/Diver/Seg.c
@@ -0,0 +1,23 @@
+#include <Seg.h>
+
+                            // 0   1   2     3    4    5    6    7   8    9   10A  11B   12C 13D   14E  15F  16-  17Ãð 18P  19U
+unsigned char seg_dula[] = {0XC0,0XF9,0XA4,0XB0,0X99,0X92,0X82,0XF8,0X80,0X90,0X88,0X83,0XC6,0XA1,0X86,0X8E,0XBF,0XFF,0X8C,0XC1};
+unsigned char seg_wela[] = {0x01,0x02,0x04,0x08,0x10,0x20,0x40,0x80};
+
+void Seg_Disp(unsigned char wela, unsigned char dula, unsigned char point)
+{
+	//ÏûÓ°
+	P0 = 0xff;
+	P2 = P2 & 0x1f | 0xe0;
+	P2 &= 0x1f;
+
+	P0 = seg_wela[wela];
+	P2 = P2 & 0x1f | 0xc0;
+	P2 &= 0x1f;
+	
+	P0 = seg_dula[dula];
+	if(point)
+		P0 &= 0x7f;
+	P2 = P2 & 0x1f | 0xe0;
+	P2 &= 0x1f;	
+}
